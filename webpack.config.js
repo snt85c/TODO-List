7d93066defb9263f6a371162668e0f9538bfacce
santi <santi.cacciola@hotmail.it>
2022-02-28 14:25:12 +0000

    updated webpack.config.js with recent folder changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
     entry: './src/script.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
 };
